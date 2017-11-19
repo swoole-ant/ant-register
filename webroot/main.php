ad834be594ac08bacba3ed83d0274c27b87adec3
shenzhe <shenzhe163@gmail.com>
2017-11-19 12:24:31 +0800

    use composer

--- webroot/main.php
+++ webroot/main.php
@@ -3,5 +3,5 @@
 use ZPHP\ZPHP;
 
 $rootPath = dirname(__DIR__);
-require 'zphp' . DIRECTORY_SEPARATOR . 'ZPHP' . DIRECTORY_SEPARATOR . 'ZPHP.php';
+require 'vendor/autoload.php';
 ZPHP::run($rootPath, true, "register");
